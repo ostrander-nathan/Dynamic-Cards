28eef0280e6f8a7f140a6a3187c872adb194d77c
Nathan Ostrander <ostrander_nathan@yahoo.com>
2017-04-26 15:45:59 -0500

    clean

--- cards.js
+++ cards.js
@@ -1,13 +1,13 @@
  document.getElementById('btn').addEventListener('click', function() {
-   var value = document.getElementById("input").value;
-   var textarea = document.createElement('div');
-   textarea.className = "mytextbox";
-   textarea.innerHTML = value;
-   var btnDelete = document.createElement("button");
-   btnDelete.innerHTML = "Delete";
-   btnDelete.addEventListener("click", function() {
-     this.parentNode.remove();
-   });
-   textarea.appendChild(btnDelete);
-   document.getElementById('output').appendChild(textarea);
+     var value = document.getElementById("input").value;
+     var textarea = document.createElement('div');
+     textarea.className = "mytextbox";
+     textarea.innerHTML = value;
+     var btnDelete = document.createElement("button");
+     btnDelete.innerHTML = "Delete";
+     btnDelete.addEventListener("click", function() {
+         this.parentNode.remove();
+     });
+     textarea.appendChild(btnDelete);
+     document.getElementById('output').appendChild(textarea);
  })
